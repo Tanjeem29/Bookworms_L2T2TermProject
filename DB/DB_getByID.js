813b607048aae02c219aa07e50a531c3074cd4f5
Tanjeem29 <azwadtanjeem@gmail.com>
2022-02-10 09:55:17 +0600

    BOOK + AUTHOR LINKS ADDED (MAJOR)
    Can now follow search results to pages

---
+++ DB/DB_getByID.js
@@ -0,0 +1,35 @@
+const db = require('./DB_Basics');
+
+
+async function getByBookID(strin){
+    const sql = `
+    SELECT * FROM BOOKS B
+    WHERE B.BOOK_ID = :BookID
+    `;
+    const binds = {
+        BookID : strin
+    }
+
+    return (await db.execute(sql, binds, db.options)).rows;
+}
+
+
+//console.log(getByBookID(1));
+
+async function getByAuthorID(strin){
+    const sql = `
+    select * from AUTHOR
+    where AUTHOR.AUTHOR_ID = :AID
+    `;
+    const binds = {
+        AID : strin
+    }
+
+    return (await db.execute(sql, binds, db.options)).rows;
+}
+
+module.exports =
+{ 
+    getByBookID,
+    getByAuthorID
+}
--- routes/bookRoutes/mainbookRoutes.js
+++ routes/bookRoutes/mainbookRoutes.js
@@ -13,6 +13,7 @@ const router = express.Router({mergeParams : true});
 const DB = require(process.env.ROOT + '\\DB\\DB_Basics');
     DB.startup();
     const DB_Searches = require(process.env.ROOT + '\\DB\\DB_Searches');
+    const DB_getByID = require(process.env.ROOT + '\\DB\\DB_getByID');
 
 
 router.get('/books', (req,res)=>{
@@ -42,6 +43,34 @@ router.get('/books/search', (req,res)=>{
     }
     else{
         res.redirect('/books');
+    }
+    
+});
+
+
+router.get('/books/:id', async (req,res)=>{
+    session = req.session;
+    //No Login access tried, so redirect to login
+    if(!session.userid){
+        console.log('NO SESSION!!!!!');
+        res.redirect('/login');
+    }
+    else{
+        const id = req.params.id;
+        //console.log(id);
+        let results;
+        //results = DB_getByID;
+        results = await DB_getByID.getByBookID(id);
+        //console.log(results);
+        res.render('layout.ejs', {
+            title : 'Books',
+            body : ['OneBookPage','partials/navbar/navbar'],
+            user : null,
+            book: results[0]
+            //books
+            //errors : errors
+        })
+
     }
     
 });
